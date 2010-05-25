a97d33052704c6c8cf07454bd07639aba0f64f0d
Florian Ragwitz <rafl@debian.org>
2010-05-25 20:53:20 +0200

    Properly trim whitespace in the tag parser

--- lib/DBIx/Class/Tags/TagParser.pm
+++ lib/DBIx/Class/Tags/TagParser.pm
@@ -1,13 +1,14 @@
 package DBIx::Class::Tags::TagParser;
 
 use Moose;
+use Text::Trim;
 use namespace::autoclean;
 
 sub parse {
     my ($self, $tag_str) = @_;
     return map {
         +{ name => $_ }
-    } split /\s*,\s*/, $tag_str;
+    } trim split /,/, $tag_str;
 }
 
 __PACKAGE__->meta->make_immutable;
--- t/basic.t
+++ t/basic.t
@@ -20,7 +20,7 @@ my $foo = $foos->create({
 
 is_deeply([$foo->labels], []);
 
-$foo->add_to_labels('bar,baz');
+$foo->add_to_labels(' bar, baz ');
 
 is_deeply(
     [sort map { $_->name } $foo->labels],
